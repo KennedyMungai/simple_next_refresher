94a6bbded9e210f94133fc0af6fe8ad893f97e94
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 15:23:28 +0300

    Moved the Image tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+
 type Props = {}
 
 const Header = (props: Props) => {
-	return <header>Header</header>
+	return (
+		<header>
+			<div className=''></div>
+			<Image
+				priority
+				height={400}
+				width={2000}
+				className=''
+				src={}
+				alt=''
+				aria-label=''
+			/>
+		</header>
+	)
 }
 
 export default Header
