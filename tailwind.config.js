bf420c2bce2d4dfb10e191fca08b151415c6354a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 14:51:01 +0300

    Added some theming colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
+		colors: {
+			black: '#191A19',
+			blue: '#557AFF',
+			'blue-light': '#EFF3FF',
+			grey: '#D9D9D9',
+			'grey-light': '#FAFAFA',
+			green: '#68F590',
+			white: '#FFFFFF'
+		},
 		extend: {}
 	},
 	plugins: []
