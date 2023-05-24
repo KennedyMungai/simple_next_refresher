372c684d2f86cb68df51c689b48e091dc6a3be13
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 16:25:44 +0300

    Added a bunch of styles to the component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,31 +14,31 @@ type Props = {
 
 const Header = ({ name, pages }: Props) => {
 	return (
-		<header>
-			<div className=''>
-				<nav>
+		<header className='flex flex-col relative bg-linear-557AFF w-full h-48'>
+			<div className='flex flex-col justify-around h-full px-6 bg-header z-10'>
+				<nav className='text-right flex flex-row gap-6 self-end'>
 					{pages.map((page) => {
 						return (
 							<Link
 								aria-label={page.label}
 								key={page.id}
 								href={page.link}
-								className=''
+								className='px-6 py-2 min-w-100 hover:bg-blue-light hover:text-blue font-display uppercase tracking-wide'
 							>
 								{page.label}
 							</Link>
 						)
 					})}
 				</nav>
-				<h1>{name}</h1>
+				<h1 className='text-white uppercase'>{name}</h1>
 			</div>
 			<Image
 				priority
 				height={400}
 				width={2000}
-				className=''
+				className='w-full h-48 object-cover object-top absolute'
 				src={'/images/jeshoots-com-pUAM5hPaCRI-unsplash.jpg'}
-				alt=''
+				alt='Some Image'
 				aria-label=''
 			/>
 		</header>
