637d62b3661fc47caeae0c0964edc5adcd431acb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 09:38:05 +0300

    Added some markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,24 @@
+import { Envelope, Linkedin, Medium, Twitter } from '../../public/icons'
+
 type Props = {}
 
 const Footer = (props: Props) => {
-    return (
-		<footer>
-			<div></div>
+	return (
+		<footer className='w-full bg-white shadow-footer mt-12 py-4 fixed bottom-0'>
+			<div>
+				<a href={} aria-label={'Email'}>
+					<Envelope className={} />
+				</a>
+				<a href={} aria-label={'Twitter'}>
+					<Twitter className={} />
+				</a>
+				<a href={} aria-label={'LinkedIn'}>
+					<Linkedin className={} />
+				</a>
+				<a href={} aria-label={'Medium'}>
+					<Medium className={} />
+				</a>
+			</div>
 		</footer>
 	)
 }
