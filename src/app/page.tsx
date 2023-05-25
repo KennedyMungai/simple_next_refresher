023cb028507c62e84ccd96b7bad7e729243ec4db
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 12:52:25 +0300

    Made some more styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
 	return (
 		<main className='flex flex-col items-center mb-32 p-10'>
-			<section className='flex sm:flex-row w-10/12 mt-10 items-center mb-6 flex-col'>
+			<section className='flex sm:flex-row sm:w-10/12 mt-10 items-center mb-6 flex-col w-full'>
 				<div className='sm:w-1/3 w-full'>
 					<Image
 						priority={false}
