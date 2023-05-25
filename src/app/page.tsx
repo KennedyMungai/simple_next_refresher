0220f80b0f0e3b14c469e44636128d503e67d9f6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 10:18:56 +0300

    Added a blank image tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { aboutMeData } from '@/components/data/data'
+import Image from 'next/image'
 
 export default function Home() {
 	return (
 		<main className='flex flex-col items-center'>
 			<section className='flex flex-row w-10/12 mt-10 items-center mb-6'>
-				<div>Image</div>
+				<div className='w-1/3'>
+					<Image
+						width={}
+						height={}
+						aria-label={}
+						alt=''
+						src={}
+						priority={false}
+					/>
+				</div>
 				<div>
 					<h2>{aboutMeData.title}</h2>
 					<p>{aboutMeData.body}</p>
