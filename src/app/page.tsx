aa1454b2429e300a14c2486586a74eeb5d00a3f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 09:52:28 +0300

    Set up the default layout of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <main></main>
+  return (
+		<main>
+			<section>
+				<div>Image</div>
+				<div>Text</div>
+			</section>
+			<section></section>
+			<section>
+				<div>Text</div>
+				<div>Image</div>
+			</section>
+		</main>
+  )
 }
