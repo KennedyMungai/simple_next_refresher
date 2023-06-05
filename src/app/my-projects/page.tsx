d10ef4ad1aa3d2a846ca7c60b844c55bd6e93590
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 14:46:33 +0300

    Printed out the project titles

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -7,6 +7,9 @@ const MyProjects = (props: Props) => {
 		<div className='flex flex-col w-11/12 content-center m-auto'>
 			<h2 className='text-2xl font-bold mb-4'>My Projects</h2>
 			<div className='flex flex-row flex-wrap gap-10 mt-6'>
+				{projects.map((project) => {
+					return <p key={project.title}>{project.title}</p>
+				})}
 				<p>My Project</p>
 			</div>
 		</div>
