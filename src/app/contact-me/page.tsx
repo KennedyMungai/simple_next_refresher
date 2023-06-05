44a3c9826de6f09dbef46795626dc9c1f01c23f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 15:54:59 +0300

    Gave the page a simple layout

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,7 +1,17 @@
+import { contactMeText } from '../../components/data/data'
+
 type Props = {}
 
 const ContactMePage = (props: Props) => {
-	return <div>page</div>
+	return (
+		<div>
+			<h2> CONTACT ME</h2>
+			<div>
+				<h3>{contactMeText.title}</h3>
+				<p>{contactMeText.body}</p>
+			</div>
+		</div>
+	)
 }
 
 export default ContactMePage
