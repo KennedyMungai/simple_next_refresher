e70077e823b4238e7fe751e789567e8419ac767a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 14:50:30 +0300

    Added some items to the prope type

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,4 +1,9 @@
-type Props = {}
+type Props = {
+	image: string
+    description: string
+    tech_stack: string
+	title: string
+}
 
 const ProjectCard = (props: Props) => {
 	return <div>project-card</div>
