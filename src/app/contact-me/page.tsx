6eb91e82a76f98f3551f5dd9fdf6e2be80f2bc96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 15:57:24 +0300

    Made the inner div use a flex and change its orientation to column

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -6,7 +6,7 @@ const ContactMePage = (props: Props) => {
 	return (
 		<div className='flex flex-col'>
 			<h2> CONTACT ME</h2>
-			<div className='w-11/12 content-center m-auto'>
+			<div className='flex flex-col w-11/12 content-center m-auto'>
 				<h3>{contactMeText.title}</h3>
 				<p>{contactMeText.body}</p>
 			</div>
