6119079fd943f15473d79b5683cde86cbd260d2a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 16:21:58 +0300

    Improved the label styling

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,7 +8,7 @@ const Form = ({ onSubmit }: Props) => {
 	return (
 		<form onSubmit={onSubmit}>
 			<div className='mb-4'>
-				<label htmlFor='name' className='font-bold mb-2'>
+				<label htmlFor='name' className='label-form'>
 					Name
 				</label>
 				<input type='text' id='name' className='input-form' />
