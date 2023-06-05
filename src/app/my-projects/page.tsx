f444ed4bfe6ce2ff61754aa9f0157580eb77a3f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 14:43:23 +0300

    Added some simple markup for the page

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -1,7 +1,14 @@
 type Props = {}
 
 const MyProjects = (props: Props) => {
-	return <div>MyProjects</div>
+	return (
+		<div className='flex flex-col w-11/12 content-center m-auto'>
+			<h2 className='text-2xl font-bold mb-4'>My Projects</h2>
+			<div className='flex flex-row flex-wrap gap-10 mt-6'>
+				<p>My Project</p>
+			</div>
+		</div>
+	)
 }
 
 export default MyProjects
