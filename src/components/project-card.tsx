9b6765b47172571031ff8aebac893b07bc6099b2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 14:56:45 +0300

    Added the description section of theh cards

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -19,6 +19,21 @@ const ProjectCard = ({ image, description, tech_stack, title }: Props) => {
 				alt={title}
 				className=''
 			/>
+			<div>
+				<h3 className='text-lg font-medium'>{title}</h3>
+				<p className='text-sm mt-2 min-h-50'>{description}</p>
+				<p className='text-sm mt-2 min-h-50'>{tech_stack}</p>
+				<div className='flex flex-row mt-4 justify-between'>
+					<a href={} className='flex items-center'>
+						{/* Github Icon */}
+						<span className='text-black'>Source Code</span>
+					</a>
+					<a href={} className='flex items-center'>
+						{/* Website Icon */}
+						<span className='text-black'>Source Code</span>
+					</a>
+				</div>
+			</div>
 		</div>
 	)
 }
