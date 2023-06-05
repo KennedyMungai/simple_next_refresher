35283262befd85712798ef093e8183e5bcec3c77
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 16:04:28 +0300

    Added a gap between the flex items

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -6,7 +6,7 @@ const ContactMePage = (props: Props) => {
 	return (
 		<div className='flex flex-col'>
 			<h2> CONTACT ME</h2>
-			<div className='flex flex-row w-11/12 content-center m-auto bg-grey-light p-10'>
+			<div className='flex flex-row w-11/12 content-center m-auto bg-grey-light p-10 gap-2'>
 				<div className='w-1/2'>
 					<h3>{contactMeText.title}</h3>
 					<p>{contactMeText.body}</p>
