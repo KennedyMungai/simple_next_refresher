d110b1402691ac6548b0b3ad89be4039be1acfaa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 15:19:20 +0300

    Rendered out the projects in card form

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -9,7 +9,16 @@ const MyProjects = (props: Props) => {
 			<h2 className='text-2xl font-bold mb-4'>My Projects</h2>
 			<div className='flex flex-row flex-wrap gap-10 mt-6'>
 				{projects.map((project) => {
-					return <p key={project.title}>{project.title}</p>
+					return (
+						<ProjectCard
+							image={project.image}
+							description={project.body1}
+							tech_stack={project.body2}
+							links={project.links}
+							title={project.title}
+							key={project.title}
+						/>
+					)
 				})}
 				<p>My Project</p>
 			</div>
