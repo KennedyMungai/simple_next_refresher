43e5dd3840bd9ddb0d55e1f7821dd5d6b09e4234
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 16:01:14 +0300

    Updated the flex to use row instead of column

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -6,7 +6,7 @@ const ContactMePage = (props: Props) => {
 	return (
 		<div className='flex flex-col'>
 			<h2> CONTACT ME</h2>
-			<div className='flex flex-col w-11/12 content-center m-auto bg-grey-light p-10'>
+			<div className='flex flex-row w-11/12 content-center m-auto bg-grey-light p-10'>
 				<div>
 					<h3>{contactMeText.title}</h3>
 					<p>{contactMeText.body}</p>
