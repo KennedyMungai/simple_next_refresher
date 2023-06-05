b1c740dccee9e2ee37897d544a611f6262aeebed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-05 14:53:00 +0300

    Added an image to the tag

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,12 +1,26 @@
+import Image from 'next/image'
+
 type Props = {
 	image: string
-    description: string
-    tech_stack: string
+	description: string
+	tech_stack: string
 	title: string
 }
 
-const ProjectCard = (props: Props) => {
-	return <div>project-card</div>
+const ProjectCard = ({ image, description, tech_stack, title }: Props) => {
+	return (
+		<div>
+			<Image
+				aria-label={}
+				priority={false}
+				width={500}
+				height={250}
+				src={image}
+				alt={title}
+				className=''
+			/>
+		</div>
+	)
 }
 
 export default ProjectCard
