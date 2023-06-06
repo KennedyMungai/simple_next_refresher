26a99c59de701c4a1d83093f1d72798e51f304ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-06 16:15:16 +0300

    Simple rendering test logic for the render of the MyProjects component

--- src/components/tests/page.spec.tsx
+++ src/components/tests/page.spec.tsx
@@ -1,3 +1,10 @@
 import MyProjects from '@/app/my-projects/page'
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
+
+
+describe('My Projects Page', () => {
+	it('should render correctly', () => {
+		render(<MyProjects />)
+	})
+})
