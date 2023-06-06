af1d0ae4da4d9da9b4e0141bc54451bae35d79f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-06 15:46:13 +0300

    Wrote a basic test for rendering the component on the client's computer

--- src/app/page.spec.tsx
+++ src/app/page.spec.tsx
@@ -1,3 +1,10 @@
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import Home from './page'
+
+
+describe('Home Page', () => {
+	it('Should render without crashing', () => {
+		render(<Home />)
+	})
+})
