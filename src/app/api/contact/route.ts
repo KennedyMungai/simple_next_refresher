2b75449cff8d03bc829fe209642e43b7e69b05fa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-06 14:49:14 +0300

    Some simple backend logic

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,26 @@ export async function POST(request)
 {
     const data = request.json()
 
-    const filePath = path.resolve(process.cwd(), )
+    const filePath = path.resolve(process.cwd(), 'app/data/submission.json')
+
+    let submissions: any = []
+
+    try {
+        const data = fs.readFileSync(filePath, 'utf8')
+        submissions = JSON.stringify(data)
+    
+    } catch (error: any) {
+        console.error('Error Reading this file. Message - '+error.message)
+    }
+
+    submissions.push(data)
+
+    try {
+        const newData = JSON.stringify(submissions, null, 2)
+        fs.writeFileSync(filePath, newData, 'utf8')
+    } catch (error: any) {
+        console.error('Error Writing this file. Message - '+ error.message)
+    }
 }
 
 // export async function GET()
