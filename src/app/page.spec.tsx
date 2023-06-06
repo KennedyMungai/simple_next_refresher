ec5b921f4085889fcaf1a584f0945d633e6af532
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-06 15:49:13 +0300

    Wrote a simple assertion for the title of the home page

--- src/app/page.spec.tsx
+++ src/app/page.spec.tsx
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Home from './page'
-
 
 describe('Home Page', () => {
 	it('Should render without crashing', () => {
 		render(<Home />)
+
+		const title = screen.getByText(/Kijana Mang'aa/)
+
+		expect(title).toBeInTheDocument()
 	})
 })
