09e56e75e5549b413c1bfaef6bf472ce0e374d86
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-06 16:16:54 +0300

    Added a commit and an assetion

--- src/components/tests/page.spec.tsx
+++ src/components/tests/page.spec.tsx
@@ -1,10 +1,12 @@
 import MyProjects from '@/app/my-projects/page'
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-
+import { render, screen } from '@testing-library/react'
 
 describe('My Projects Page', () => {
 	it('should render correctly', () => {
 		render(<MyProjects />)
+
+		const title = screen.getByText('My Projects')
+		expect(title).toBeInTheDocument()
 	})
 })
