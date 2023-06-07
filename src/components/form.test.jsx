6bd85e162e2bcc6b16b96f9dd9523b7b7d5363b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-07 09:26:33 +0300

    Changed the individual test

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -2,14 +2,6 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Form from './Form'
 
-describe('My Projects Page', () => {
-	it('should render correctly', () => {
-		render(<Form />)
-
-		const button = screen.getAllByRole('button', { name: 'Send Message' })
-		expect(button).toBeInTheDocument()
-
-		const title = screen.getByText('My Projects')
-		expect(title).toBeInTheDocument()
-	})
+describe('The Contact Form Component', () => {
+	it('Should submit the form and show a successful message', () => {})
 })
