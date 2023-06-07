04b4cfba21d474c20fa8e77ce9f7d93409c21dc3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-07 09:45:38 +0300

    Imported setupServer from msw-node

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen, fireEvent, waitFOr } from '@testing-library/react'
 import Form from './Form'
+import { setupServer } from 'msw-node'
 
 describe('The Contact Form Component', () => {
 	it('Should submit the form and show a successful message', async () => {
