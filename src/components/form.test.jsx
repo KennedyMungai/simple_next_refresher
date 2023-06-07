a6df8945d4a72b1c5c78817840d0944734d4524f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-07 09:27:23 +0300

    Rendered the Form component, imported fireEvent and waitFOr

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -1,7 +1,9 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent, waitFOr } from '@testing-library/react'
 import Form from './Form'
 
 describe('The Contact Form Component', () => {
-	it('Should submit the form and show a successful message', () => {})
+	it('Should submit the form and show a successful message', () => {
+		render(<Form />)
+	})
 })
