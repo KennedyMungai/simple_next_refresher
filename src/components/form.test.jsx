dc3463bd9b8be0d6f120604812792f274ec27652
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-07 09:23:44 +0300

    Wrote the test for the code

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -1,2 +1,15 @@
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import Form from './Form'
+
+describe('My Projects Page', () => {
+	it('should render correctly', () => {
+		render(<Form />)
+
+		const button = screen.getAllByRole('button', { name: 'Send Message' })
+		expect(button).toBeInTheDocument()
+
+		const title = screen.getByText('My Projects')
+		expect(title).toBeInTheDocument()
+	})
+})
