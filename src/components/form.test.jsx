838c78e3148fbddcee75b2ee3d3c4fc77169b62f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-07 10:44:45 +0300

    Reorganized the imports for the component

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import 'isomorphic-fetch'
 import { rest } from 'msw'
 import { setupServer } from 'msw-node'
 import Form from './Form'
-import 'isomorphic-fetch'
 
 
 const handlers = [
